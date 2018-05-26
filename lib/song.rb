c19533741b20a1e24eb0f650aa966f0b820b531d
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-26 21:02:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
 
   @@count = 0
-
+  @@artists = []
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
@@ -9,11 +9,15 @@ class Song
     @artist = artist
     @genre = genre
     @@count = @@count +1
+    @@artists << name unless @@artists.include?(name)
 
   end
 
   def self.count
     @@count
+  end
+  def self.artists
+    @@artists
   end
 
 end
