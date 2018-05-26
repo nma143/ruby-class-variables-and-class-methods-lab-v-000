405916d18b6216a63ff166b17f4a46398861edea
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-26 21:45:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,11 +31,10 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count.keys.include?(genre)
-        genre_count[genre] = genre_count[genre] +1 
+        genre_count[genre] = genre_count[genre] +1
       else
         genre_count[genre] = 1
       end
-      
     end
 
   end
