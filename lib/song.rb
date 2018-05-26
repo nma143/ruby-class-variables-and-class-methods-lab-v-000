68f5312e5eada50840277e3cea37423643cd0a41
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-26 21:47:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,4 +39,19 @@ class Song
     return genre_count
   end
 
+  def self.artist_count
+
+    artist_count = {}
+    @@artist.each do |artist|
+      if artist_count.keys.include?(artist)
+        artist_count[artist] = artist_count[artist] +1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    return artist_count
+  end
+
+  
+
 end
